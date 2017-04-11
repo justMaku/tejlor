8b1f86efaf488f6a7d7e92f052688ebf1a3105f8
Michał Kałużny <maku@justmaku.org>
2017-04-11 03:23:29 +0200

    Changes needed for it to ~work~ on linux

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,42 +1,25 @@
+import Foundation
 
-//let connection = try? Connection(server: Server(host: "irc.freenode.net", port: 6667))
-//try? connection?.connect()
-//print("Connected")
-//
-//while true {
-//    print("Command: ")
-//    guard let command = readLine() else {
-//        break
-//    }
-//    try? connection?.send(command: command)
-//}
+let queue = OperationQueue()
+queue.qualityOfService = .utility
 
-import Rexy
-
-//do {
-//    let RFC2812ParsingRegexp = try Regex("^(?::(([^@! ]*)(?:(?:!([^@]*))?@([^ ]*))?) )?([^ ]+)((?: [^: ][^ ]*){0,14})(?: :?(.*))?$", flags: [.extended, .newLineSensitive])
-//} catch let error {
-//    print(error)
-//}
-
-
-let irc = try! IRC(server: IRC.Server(host: "irc.freenode.net", port: 6667))
-let hello = HelloPlugin()
-let terms = TermsPlugin()
-
-let bot = Bot(name: "tejlor")
-
-bot.register(irc)
-bot.register(hello)
-bot.register(terms)
-
-try? bot.connect()
-
-while true {
-    let command = readLine()
-    if command == "q" {
-        break
-    }
+queue.addOperation {
+    print("Welcome to New York")
+    
+    let irc = try! IRC(server: IRC.Server(host: "irc.freenode.net", port: 6667))
+    let hello = HelloPlugin()
+    let terms = TermsPlugin()
+    
+    let bot = Bot(name: "tejlor")
+    
+    bot.register(irc)
+    bot.register(hello)
+    bot.register(terms)
+    
+    try? bot.connect()
+    
+    print("Bot started")
 }
 
+Thread.sleep(until: Date.distantFuture)
 print("Quitting now, bye")
